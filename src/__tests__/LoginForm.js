cb243697918356485aa146390e64ad7e6b9eae1b
Ricardo <ricardo_simoes_93@hotmail.com>
2020-03-12 20:17:48 +0100

    Prototype of login form + tests (#4)
    * Started working on loginForm for testing
    
    * Made LoginForm be rendered my App.js
    
    * Created a first login form
    
    * removed unnecessary keep logged in switch
    
    * reformated code
    
    removed unnecessary code
    
    * Added axios package for requests
    
    * refactored code more
    
    * Added axios login function for #3
    
    * Moved request code into a dedicated file
    
    * Delete App.test.js
    
    * small refactor
    
    * Added first tests for the react components
    
    * small refactor
    
    * added more unit tests
    
    * added github action to test code
    
    * Delete MainPage.js

---
+++ src/__tests__/LoginForm.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render, getByTestId } from '@testing-library/react';
+import LoginForm from '../components/loginForm/loginForm.';
+
+it('renders the username input field', () => {
+  const { getByText } = render(<LoginForm />);
+  const element = getByText(/username/i);
+  expect(element).toBeInTheDocument();
+});
+
+it('renders the password input field', () => {
+  const { getByText } = render(<LoginForm />);
+  const element = getByText(/password/i);
+  expect(element).toBeInTheDocument();
+});
+
+it('renders the login button', () => {
+  const { getByText } = render(<LoginForm />);
+  const element = getByText(/login/i);
+  expect(element).toBeInTheDocument();
+});
+
+it('renders the logout button', () => {
+  const { getByText } = render(<LoginForm />);
+  const element = getByText(/logout/i);
+  expect(element).toBeInTheDocument();
+});
