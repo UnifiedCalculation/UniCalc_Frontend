03cb9f3c9629a8ebf77f6c72364e3145b9b3d005
Ricardo <ricardo_simoes_93@hotmail.com>
2020-04-01 18:01:10 +0200

    Fixed unit tests (#12)
    * Started working on loginForm for testing
    
    * Made LoginForm be rendered my App.js
    
    * Created a first login form
    
    * removed unnecessary keep logged in switch
    
    * reformated code
    
    removed unnecessary code
    
    * Added axios package for requests
    
    * refactored code more
    
    * Added axios login function for #3
    
    * Moved request code into a dedicated file
    
    * Delete App.test.js
    
    * small refactor
    
    * Added first tests for the react components
    
    * small refactor
    
    * added more unit tests
    
    * added github action to test code
    
    * Delete MainPage.js
    
    * Updated LoginForm for RoR usage
    
    * Added singlePage entry point for RoR
    
    * fixed typo
    
    * Removed css to not interfer
    
    * Refactored tests

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import SinglePage from './components/singlePage/singlePage';
 
 function App() {
--- src/__tests__/LoginForm.js
+++ src/__tests__/LoginForm.js
@@ -4,13 +4,13 @@ import LoginForm from '../components/loginForm/loginForm';
 
 it('renders the username input field', () => {
   const { getByText } = render(<LoginForm />);
-  const element = getByText(/username/i);
+  const element = getByText(/E-Mail/i);
   expect(element).toBeInTheDocument();
 });
 
 it('renders the password input field', () => {
   const { getByText } = render(<LoginForm />);
-  const element = getByText(/password/i);
+  const element = getByText(/Password/i);
   expect(element).toBeInTheDocument();
 });
 
