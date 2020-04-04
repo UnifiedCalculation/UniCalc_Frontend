7a8f6ecc3793cc35a0a86df49f94ff0808ca6daa
Ricardo Monteiro Simoes <ricardo_simoes_93@hotmail.com>
2020-04-04 16:25:59 +0200

    Removed unneeded imports

--- src/components/newProjectDialog/newProjectDialog.js
+++ src/components/newProjectDialog/newProjectDialog.js
@@ -7,11 +7,9 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
-import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
 import TextField from '@material-ui/core/TextField';
 import InputLabel from '@material-ui/core/InputLabel';
-import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import { makeStyles } from '@material-ui/core/styles';
 
