a46013cd72427fcbb0ceea15f6aa9ddcde76a635
Adrian Vieceli <vieceadr@students.zhaw.ch>
2020-04-04 17:52:58 +0200

    tried to change the colors of the icons in the bottom bar, doesn't work. added link to docu

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import LoginForm from './components/loginForm/loginForm.';
+import './components/layouts/navigation'
+import { BottomNavigation } from '@material-ui/core';
+import Navigation from "./components/layouts/navigation";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <LoginForm />
-      </header>
+      // Add your code here
+      <Navigation></Navigation>
     </div>
   );
 }
