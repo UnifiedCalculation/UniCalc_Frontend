fc8e5e104da2de04807e6041370a248d15040919
Adrian Vieceli <vieceadr@students.zhaw.ch>
2020-04-05 15:36:45 +0200

    created header

--- src/components/singlePage/singlePage.js
+++ src/components/singlePage/singlePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navigation from '../layouts/navigation'
+import Header from "../layouts/header/header";
 
 const SinglePage = () => {
 
     return (
         <>
+          <Header/>
           <p>Nothing yet...</p>
           <Navigation />
         </>
