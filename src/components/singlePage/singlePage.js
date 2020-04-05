536cee113057e0617c47234e5f7c96493b7ddd2f
adrianvie <60985784+adrianvie@users.noreply.github.com>
2020-04-05 16:08:31 +0200

    Dashboard (#20)
    * added dashboard including tables, rows aren't correct yet
    
    * Tables with correct titles
    
    * added dashboard to singlePage.js
    
    * readded Alert to singlePage.js
    
    Co-authored-by: Adrian Vieceli <vieceadr@students.zhaw.ch>
    Co-authored-by: Ricardo <ricardo_simoes_93@hotmail.com>

--- src/components/singlePage/singlePage.js
+++ src/components/singlePage/singlePage.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import Alert from '../Alert/alert';
 import Navigation from '../layouts/navigation'
+import Dashboard from "../layouts/dashboard/dashboard";
 
 const SinglePage = () => {
 
-    return (
-        <>
-          <p>Nothing yet...</p>
-          <Navigation />
-        </>
-    );
+  return (
+      <>
+        <p>Nothing yet...</p>
+        <Navigation />
+      </>
+  );
 }
 export default SinglePage;
