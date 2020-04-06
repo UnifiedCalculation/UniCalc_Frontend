76eb330f567357961d0d942ea39d652be60ccfa8
Ricardo Monteiro Simoes <ricardo_simoes_93@hotmail.com>
2020-04-06 21:31:54 +0200

    Refactored small naming error

--- src/components/selectArticleDialog/selectArticleDialog.js
+++ src/components/selectArticleDialog/selectArticleDialog.js
@@ -7,7 +7,10 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
+import Select from '@material-ui/core/Select';
 import TextField from '@material-ui/core/TextField';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
 import { makeStyles } from '@material-ui/core/styles';
 
 import useMediaQuery from '@material-ui/core/useMediaQuery';
@@ -19,18 +22,39 @@ import { useTheme } from '@material-ui/core/styles';
  * @param {Function} onSubmit 
  * @param {Boolean} show
  */
-const NewOfferDialog = ({ customers, onCancel, onSubmit, show, ...props }) => {
+const SelectArticleDialog = ({ /*articles,*/ onCancel, onSubmit, show, ...props }) => {
 
     const cancelText = 'Abbrechen';
     const acceptText = 'Bestätigen';
-    const title = 'Neue Offerte erstellen';
-    const text = 'Tragen Sie bitte alle Felder ein, um eine neue Offerte zu erstellen.';
+    const title = 'Artikel hinzufüge';
+    const text = 'Alle Felder eintragen, um einen Artikel hinzuzufügen';
+
+    const articles = [
+        {
+            name: "Steckdose T13 3-fach UP weiss",
+            article_id: 1234123,
+            unit: "Stk.",
+            price: 125.80,
+        },
+        {
+            name: "Steckdose T13 3-fach AP weiss",
+            article_id: 1234123,
+            unit: "Stk.",
+            price: 220.25,
+        }
+    ]
 
     const textfields = [
         {
-            id: 'offername',
-            label: 'Offertenbezeichnung',
-            type: 'text',
+            id: 'amount',
+            label: 'Anzahl',
+            type: 'number',
+            required: true
+        },
+        {
+            id: 'discount',
+            label: 'Rabatt',
+            type: 'number',
             required: true
         },
         {
@@ -40,6 +64,19 @@ const NewOfferDialog = ({ customers, onCancel, onSubmit, show, ...props }) => {
             required: true
         }
     ];
+
+    let emptyArticlesList = [];
+    emptyNameSelection.push(<option id="emptyOption" key="0-option"></option>);
+    const articlesSelection = customers ? emptyNameSelection.concat(
+        customers.map((entry, index) =>
+            <option
+                value={entry.article_id}
+                key={(index + 1) + '-option'}
+            >
+                {entry.name}
+            </option >
+        )
+    ) : emptyArticlesList;
 
     const inputFields = textfields.map((entry, index) =>
         <TextField
@@ -55,6 +92,17 @@ const NewOfferDialog = ({ customers, onCancel, onSubmit, show, ...props }) => {
         />
     );
 
+    const useStyles = makeStyles((theme) => ({
+        formControl: {
+            margin: theme.spacing(0),
+            minWidth: 120,
+        },
+        selectEmpty: {
+            marginTop: theme.spacing(5),
+        },
+    }));
+
+    const classes = useStyles();
     const theme = useTheme();
     const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
@@ -83,7 +131,24 @@ const NewOfferDialog = ({ customers, onCancel, onSubmit, show, ...props }) => {
                     <DialogContentText id="new-project-dialog-description">
                         {text}
                     </DialogContentText>
-                    <form id='newOfferForm' onSubmit={prepareProjectData}>
+                    <form id='newArticleForm' onSubmit={prepareProjectData}>
+                        <FormControl
+                            required
+                            className={classes.formControl}
+                            fullWidth
+                        >
+                            <InputLabel id="required-select-autowidth-label">Kunde</InputLabel>
+                            <Select
+                                native
+                                labelId="required-select-autowidth-label"
+                                id="article_id"
+                                name="article_id"
+                                fullWidth
+                                margin='dense'
+                            >
+                                {customerSelector}
+                            </Select>
+                        </FormControl>
                         {inputFields}
                     </form>
                 </DialogContent>
@@ -91,7 +156,7 @@ const NewOfferDialog = ({ customers, onCancel, onSubmit, show, ...props }) => {
                     <Button onClick={onCancel} color="primary">
                         {cancelText}
                     </Button>
-                    <Button type="submit" form='newOfferForm' color="primary" autoFocus>
+                    <Button type="submit" form='newArticleForm' color="primary" autoFocus>
                         {acceptText}
                     </Button>
                 </DialogActions>
@@ -100,10 +165,11 @@ const NewOfferDialog = ({ customers, onCancel, onSubmit, show, ...props }) => {
     );
 }
 
-NewOfferDialog.propTypes = {
+SelectArticleDialog.propTypes = {
+    customers: PropTypes.array.isRequired, 
     onCancel: PropTypes.func.isRequired,
     onSubmit: PropTypes.func.isRequired,
     show: PropTypes.bool.isRequired
 }
 
-export default NewOfferDialog;
+export default SelectArticleDialog;
