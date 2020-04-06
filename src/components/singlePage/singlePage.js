e2e339219ae6fdb0c85cd2c0503c68d195eb316d
adrianvie <60985784+adrianvie@users.noreply.github.com>
2020-04-06 20:39:07 +0200

    cleaned warnings (#25)
    Co-authored-by: Adrian Vieceli <vieceadr@students.zhaw.ch>

--- src/components/singlePage/singlePage.js
+++ src/components/singlePage/singlePage.js
@@ -2,16 +2,26 @@ import React from 'react';
 import Navigation from '../layouts/navigation'
 import Header from "../layouts/header/header";
 import UserOptions from "../layouts/userOptions/userOptions";
+import {makeStyles} from "@material-ui/core/styles";
+import './singlePage.css'
+
+const useStyles = makeStyles({
+    }
+);
 
 const SinglePage = () => {
 
-    return (
-        <>
+  const classes = useStyles();
+
+  return (
+      <>
+        <div className={"content"}>
           <Header/>
           <p>Nothing yet...</p>
           <UserOptions/>
-          <Navigation />
-        </>
-    );
-}
+        </div>
+        <Navigation />
+      </>
+  );
+};
 export default SinglePage;
