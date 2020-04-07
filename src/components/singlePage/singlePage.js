ad10dcd3e5afd2f0c0f5ae50345d38b82da03e7f
Ricardo Monteiro Simoes <ricardo_simoes_93@hotmail.com>
2020-04-07 17:16:57 +0200

    Added functional states

--- src/components/singlePage/singlePage.js
+++ src/components/singlePage/singlePage.js
@@ -1,15 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navigation from '../layouts/navigation'
 import Header from "../layouts/header/header";
-import UserOptions from "../layouts/userOptions/userOptions";
-import {makeStyles} from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import './singlePage.css'
 
+import ProjectCard from '../projectCard/projectCard';
+import NewProjectDialog from '../newProjectDialog/newProjectDialog';
+import * as API from '../connectionHandler/connectionHandler';
+
 const useStyles = makeStyles({
-    }
+}
 );
 
 const SinglePage = () => {
+
+  const [projectData, setProjectData] = useState([]);
+  const [showProject, setShowProjectViewState] = useState(false);
+  const [customerData, setCustomerData] = useState([]);
+  const [showNewProjectDialog, setNewProjectDialogViewState] = useState(false);
+
+  useEffect(() => {
+    API.getUserProjects(setProjectData);
+  }, []);
+
+  const openProject = (projectId) => {
+
+
+  };
+
+  const openNewProjectDialog = () => {
+    API.getCustomers(setCustomerData);
+    setNewProjectDialogViewState(true);
+  }
+
+  const closeNewProjectDialog = () => {
+    setNewProjectDialogViewState(false);
+    setCustomerData([]);
+  }
+
+  const submitNewProject = (newProjectData) => {
+    setNewProjectDialogViewState(false);
+    console.log(JSON.stringify(newProjectData));
+  }
+
+  const addNewProjectDialog =
+    <NewProjectDialog
+      show={showNewProjectDialog}
+      customers={customerData}
+      onCancel={closeNewProjectDialog}
+      onSubmit={submitNewProject}
+    />
+
 
   const classes = useStyles();
 
