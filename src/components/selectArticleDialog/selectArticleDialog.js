d60dd6500547ca888e3df75b9e6040ae84ebaa6a
Ricardo Monteiro Simoes <ricardo_simoes_93@hotmail.com>
2020-04-21 15:10:18 +0200

    Revert "Revert "refactored to use dynamicDialog""
    This reverts commit 6251b3b56a06d78503eebda72dc654b728c2bdad.

--- src/components/selectArticleDialog/selectArticleDialog.js
+++ src/components/selectArticleDialog/selectArticleDialog.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-
 import Select from '@material-ui/core/Select';
 import TextField from '@material-ui/core/TextField';
 import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
 import { makeStyles } from '@material-ui/core/styles';
 
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import { useTheme } from '@material-ui/core/styles';
+import DynamicDialog from '../dynamicDialog/dynamicDialog';
 
 /**
  * @param {Array} articles array with articles
@@ -24,8 +16,8 @@ import { useTheme } from '@material-ui/core/styles';
  */
 const SelectArticleDialog = ({ articles, onCancel, onSubmit, show, ...props }) => {
 
-    const cancelText = 'Abbrechen';
-    const acceptText = 'Bestätigen';
+    const cancelButtonText = 'Abbrechen';
+    const acceptButtonText = 'Bestätigen';
     const title = 'Artikel hinzufügen';
     const text = 'Alle Felder eintragen, um einen Artikel hinzuzufügen.';
 
@@ -50,18 +42,47 @@ const SelectArticleDialog = ({ articles, onCancel, onSubmit, show, ...props }) =
         }
     ];
 
+    const useStyles = makeStyles((theme) => ({
+        formControl: {
+            margin: theme.spacing(0),
+            minWidth: 120,
+        },
+        selectEmpty: {
+            marginTop: theme.spacing(5),
+        },
+    }));
+
+    const classes = useStyles();
+
     let emptyArticlesList = [];
     emptyArticlesList.push(<option id="emptyOption" key="0-option"></option>);
-    const articlesSelection = articles ? emptyArticlesList.concat(
-        articles.map((entry, index) =>
-            <option
-                value={entry.article_id}
-                key={(index + 1) + '-option'}
+    const articlesSelection =
+        <FormControl
+            required
+            className={classes.formControl}
+            fullWidth
+        >
+            <InputLabel id="required-select-autowidth-label">Artikel</InputLabel>
+            <Select
+                native
+                labelId="required-select-autowidth-label"
+                id="customer_id"
+                name="customer_id"
+                fullWidth
+                margin='dense'
             >
-                {entry.name}
-            </option >
-        )
-    ) : emptyArticlesList;
+                {articles ? emptyArticlesList.concat(
+                    articles.map((entry, index) =>
+                        <option
+                            value={entry.article_id}
+                            key={(index + 1) + '-option'}
+                        >
+                            {entry.name}
+                        </option >
+                    )
+                ) : emptyArticlesList}
+            </Select>
+        </FormControl>
 
     const inputFields = textfields.map((entry, index) =>
         <TextField
@@ -76,21 +97,6 @@ const SelectArticleDialog = ({ articles, onCancel, onSubmit, show, ...props }) =
             margin='dense'
         />
     );
-
-    const useStyles = makeStyles((theme) => ({
-        formControl: {
-            margin: theme.spacing(0),
-            minWidth: 120,
-        },
-        selectEmpty: {
-            marginTop: theme.spacing(5),
-        },
-    }));
-
-    const classes = useStyles();
-    const theme = useTheme();
-    const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
-
 
     const prepareArticleData = (event) => {
         event.preventDefault();
@@ -107,50 +113,18 @@ const SelectArticleDialog = ({ articles, onCancel, onSubmit, show, ...props }) =
     };
 
     return (
-        <>
-            <Dialog
-                open={show}
-                onClose={onCancel}
-                aria-labelledby="new-project-dialog-title"
-                aria-describedby="new-project-dialog-description"
-                fullScreen={fullScreen}
-            >
-                <DialogTitle id="new-project-dialog-title">{title}</DialogTitle>
-                <DialogContent dividers={true}>
-                    <DialogContentText id="new-project-dialog-description">
-                        {text}
-                    </DialogContentText>
-                    <form id='newArticleForm' onSubmit={prepareArticleData}>
-                        <FormControl
-                            required
-                            className={classes.formControl}
-                            fullWidth
-                        >
-                            <InputLabel id="required-select-autowidth-label">Artikel</InputLabel>
-                            <Select
-                                native
-                                labelId="required-select-autowidth-label"
-                                id="article_id"
-                                name="article_id"
-                                fullWidth
-                                margin='dense'
-                            >
-                                {articlesSelection}
-                            </Select>
-                        </FormControl>
-                        {inputFields}
-                    </form>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={onCancel} color="primary">
-                        {cancelText}
-                    </Button>
-                    <Button type="submit" form='newArticleForm' color="primary" autoFocus>
-                        {acceptText}
-                    </Button>
-                </DialogActions>
-            </Dialog>
-        </>
+        <DynamicDialog
+            title={title}
+            text={text}
+            onCancel={onCancel}
+            cancelButtonText={cancelButtonText}
+            onAccept={prepareArticleData}
+            acceptButtonText={acceptButtonText}
+            show={show}
+        >
+            {articlesSelection}
+            {inputFields}
+        </DynamicDialog>
     );
 }
 
