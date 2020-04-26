6e1bdc0e9c93bf16ef48cc9d9573449d471d80eb
Ricardo Monteiro Simoes <ricardo_simoes_93@hotmail.com>
2020-04-26 14:27:27 +0200

    Removes old mock data

--- src/components/connectionHandler/connectionHandler.js
+++ src/components/connectionHandler/connectionHandler.js
@@ -37,31 +37,6 @@ export async function getUserProjects(callback) {
                 callback(res.data);
             }
         });
-    /*
-    const testProjectData = [
-        {
-            project_id: 1272,
-            project_name: "Villa am See",
-            description: "Neubau in Zürich"
-        },
-        {
-            project_id: 1273,
-            project_name: "Villa am Berg",
-            description: "Neubau in Chur"
-        },
-        {
-            project_id: 1274,
-            project_name: "Villa am See",
-            description: "Neubau in Zürich"
-        },
-        {
-            project_id: 1275,
-            project_name: "Villa am Berg",
-            description: "Neubau in Chur"
-        }
-    ]
-    callback(testProjectData);
-    */
 }
 
 export async function postNewOfferToProject(projectId, offer, callback) {
@@ -78,23 +53,6 @@ export async function getProjectData(projectId, callback) {
                 callback(res.data);
             }
         });
-    /*
-    const projectData = {
-        "id": 1,
-        "customer_id": 1,
-        "company_id": 1,
-        "name": "Testproject",
-        "address": "Teststreet 123",
-        "zip": "8001",
-        "city": "Zurich",
-        "created_at": "2020-04-07T15:44:02.921Z",
-        "updated_at": "2020-04-07T15:44:02.921Z",
-        "description": "This is a test Project",
-        "payment_target": "30 Tage"
-    }
-
-    callback(projectData);
-    */
 }
 
 export async function getOffersFromProject(projectId, callback) {
@@ -106,30 +64,7 @@ export async function getOffersFromProject(projectId, callback) {
                 callback(res.data);
             }
         });
-    /*
-    const offerData = [
-        {
-          id: 2,
-          project_id: 1,
-          name: "Offer 1",
-          created_at: "2020-04-07T17:57:37.468Z",
-          updated_at: "2020-04-07T17:57:37.468Z",
-          discount: null,
-          employee_id: null
-        },
-        {
-          id: 3,
-          project_id: 1,
-          name: "Offer 2",
-          created_at: "2020-04-07T17:58:24.185Z",
-          updated_at: "2020-04-07T17:58:24.185Z",
-          discount: null,
-          employee_id: null
-        }
-      ]
 
-    callback(offerData);
-      */
 }
 
 export async function saveOfferToProject(projectId, offer, callback) {
@@ -167,24 +102,7 @@ export async function getArticles(callback) {
         });
 
 
-    /*
-    const articles = [
-        {
-            name: "Steckdose T13 3-fach UP weiss",
-            article_id: 1234123,
-            unit: "Stk.",
-            price: 125.80,
-        },
-        {
-            name: "Steckdose T13 3-fach AP weiss",
-            article_id: 1234124,
-            unit: "Stk.",
-            price: 220.25,
-        }
-    ]
 
-    callback(articles);
-    */
 }
 
 export async function getOfferData(projectId, offerId, callback) {
@@ -197,59 +115,7 @@ export async function getOfferData(projectId, offerId, callback) {
         }
     });
 
-    /*
-    const offer = {
-        name: "offer 1",
-        entries: [
-            {
-                name: "badezimmer",
-                discount: null,
-                articles: [
-                    {
-                        name: "Steckdose T13 3-fach UP weiss",
-                        article_id: 1234123,
-                        unit: "Stk.",
-                        price: 125.80,
-                        amount: 15,
-                        discount: null
-                    },
-                    {
-                        name: "Steckdose T13 3-fach AP weiss",
-                        article_id: 1234124,
-                        unit: "Stk.",
-                        price: 220.25,
-                        amount: 120,
-                        discount: 7.35
-                    }
-                ]
-            },
-            {
-                name: "wohnzimmer",
-                discount: 50,
-                articles: [
-                    {
-                        name: "Steckdose T13 3-fach UP weiss",
-                        article_id: 1234123,
-                        unit: "Stk.",
-                        price: 125.80,
-                        amount: 235,
-                        discount: 13.37
-                    },
-                    {
-                        name: "Steckdose T13 3-fach AP weiss",
-                        article_id: 1234124,
-                        unit: "Stk.",
-                        price: 220.25,
-                        amount: 120,
-                        discount: 7.35
-                    }
-                ]
             }
-        ]
-    }
-
-    callback(offer);
-    */
 }
 
 export async function submitNewProject(projectData, callback) {
@@ -267,21 +133,3 @@ export async function getCustomers(callback) {
         }
     });
 
-    /*
-    const testCustomerData = [
-        {
-            name: "Name one",
-            customer_id: 1234
-        },
-        {
-            name: "Name two",
-            customer_id: 1235
-        },
-        {
-            name: "Name three",
-            customer_id: 1236
-        }
-    ]
-    callback(testCustomerData);
-    */
-}
