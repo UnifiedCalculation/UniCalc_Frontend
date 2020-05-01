59197aa26b3fe9e7b1e3e80703236adf5edc0e74
Adrian Vieceli <vieceadr@students.zhaw.ch>
2020-05-01 21:14:09 +0200

    adds setErrorMessage

--- src/components/layouts/articleAdministration/articleTable.js
+++ src/components/layouts/articleAdministration/articleTable.js
@@ -20,202 +20,204 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Switch from '@material-ui/core/Switch';
 import DeleteIcon from '@material-ui/icons/Delete';
 import FilterListIcon from '@material-ui/icons/FilterList';
-import {getArticles} from "../../connectionHandler/connectionHandler";
+import {getProducts} from "../../connectionHandler/connectionHandler";
 
-function createData(npk, id, title, price, unit, description) {
-  return {npk, id, title, price, unit, description};
-}
 
-const [articles, setArticles] = useState([]);
+const ArticleTable = ({setErrorMessage}) => {
 
-function fillArticles() {
-  setArticles(getArticles(setErrorMessage, setArticles));
-  createData(articles.npk, articles.id, articles.title, articles.price, articles.unit, articles.description)
-}
-
-const rows = [
-  createData('Cupcake', "305", "3.7", 67, "4.3", "hoi"),
-  createData('Cupcake', "305", "3.7", 67, "4.3", "hoi"),
-  createData('Cupcake', "305", "3.7", 67, "4.3", "hoi"),
-  createData('Cupcake', "305", "3.7", 67, "4.3", "hoi"),
-  createData('Cupcake', "305", "3.7", 67, "4.3", "hoi"),
-  createData('Cupcake', "305", "3.7", 67, "4.3", "hoi"),
-];
-
-function descendingComparator(a, b, orderBy) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
+  function createData(npk, id, title, price, unit, description) {
+    return {npk, id, title, price, unit, description};
   }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
+
+  const [products, setProducts] = useState([]);
+
+  function fillArticles() {
+    getProducts(setErrorMessage, setProducts);
+    createData(products.npk, products.id, products.title, products.price, products.unit, products.description)
   }
-  return 0;
-}
 
-function getComparator(order, orderBy) {
-  return order === 'desc'
-      ? (a, b) => descendingComparator(a, b, orderBy)
-      : (a, b) => -descendingComparator(a, b, orderBy);
-}
+  const rows = [
+    createData('Cupcake', "305", "3.7", 67, "4.3", "hoi"),
+    createData('Cupcake', "305", "3.7", 67, "4.3", "hoi"),
+    createData('Cupcake', "305", "3.7", 67, "4.3", "hoi"),
+    createData('Cupcake', "305", "3.7", 67, "4.3", "hoi"),
+    createData('Cupcake', "305", "3.7", 67, "4.3", "hoi"),
+    createData('Cupcake', "305", "3.7", 67, "4.3", "hoi"),
+  ];
 
-function stableSort(array, comparator) {
-  const stabilizedThis = array.map((el, index) => [el, index]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) return order;
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map((el) => el[0]);
-}
+  function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+    return 0;
+  }
 
-const headCells = [
-  {id: 'npk', numeric: false, disablePadding: true, label: 'NPK Gruppe'},
-  {id: 'id', numeric: false, disablePadding: false, label: 'Artikelnr.'},
-  {id: 'title', numeric: false, disablePadding: false, label: 'Artikelbez.'},
-  {id: 'price', numeric: true, disablePadding: false, label: 'Preis'},
-  {id: 'unit', numeric: false, disablePadding: false, label: 'Einheit'},
-  {id: 'description', numeric: false, disablePadding: false, label: 'Beschreibung'},
-];
+  function getComparator(order, orderBy) {
+    return order === 'desc'
+        ? (a, b) => descendingComparator(a, b, orderBy)
+        : (a, b) => -descendingComparator(a, b, orderBy);
+  }
 
-function EnhancedTableHead(props) {
-  const {classes, onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort} = props;
-  const createSortHandler = (property) => (event) => {
-    onRequestSort(event, property);
-  };
+  function stableSort(array, comparator) {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0]);
+      if (order !== 0) return order;
+      return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+  }
 
-  return (
-      <TableHead>
-        <TableRow>
-          <TableCell padding="checkbox">
-            <Checkbox
-                indeterminate={numSelected > 0 && numSelected < rowCount}
-                checked={rowCount > 0 && numSelected === rowCount}
-                onChange={onSelectAllClick}
-                inputProps={{'aria-label': 'select all desserts'}}
-            />
-          </TableCell>
-          {headCells.map((headCell) => (
-              <TableCell
-                  key={headCell.id}
-                  align={headCell.numeric ? 'right' : 'left'}
-                  padding={headCell.disablePadding ? 'none' : 'default'}
-                  sortDirection={orderBy === headCell.id ? order : false}
-              >
-                <TableSortLabel
-                    active={orderBy === headCell.id}
-                    direction={orderBy === headCell.id ? order : 'asc'}
-                    onClick={createSortHandler(headCell.id)}
+  const headCells = [
+    {id: 'npk', numeric: false, disablePadding: true, label: 'NPK Gruppe'},
+    {id: 'id', numeric: false, disablePadding: false, label: 'Artikelnr.'},
+    {id: 'title', numeric: false, disablePadding: false, label: 'Artikelbez.'},
+    {id: 'price', numeric: true, disablePadding: false, label: 'Preis'},
+    {id: 'unit', numeric: false, disablePadding: false, label: 'Einheit'},
+    {id: 'description', numeric: false, disablePadding: false, label: 'Beschreibung'},
+  ];
+
+  function EnhancedTableHead(props) {
+    const {classes, onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort} = props;
+    const createSortHandler = (property) => (event) => {
+      onRequestSort(event, property);
+    };
+
+    return (
+        <TableHead>
+          <TableRow>
+            <TableCell padding="checkbox">
+              <Checkbox
+                  indeterminate={numSelected > 0 && numSelected < rowCount}
+                  checked={rowCount > 0 && numSelected === rowCount}
+                  onChange={onSelectAllClick}
+                  inputProps={{'aria-label': 'select all desserts'}}
+              />
+            </TableCell>
+            {headCells.map((headCell) => (
+                <TableCell
+                    key={headCell.id}
+                    align={headCell.numeric ? 'right' : 'left'}
+                    padding={headCell.disablePadding ? 'none' : 'default'}
+                    sortDirection={orderBy === headCell.id ? order : false}
                 >
-                  {headCell.label}
-                  {orderBy === headCell.id ? (
-                      <span className={classes.visuallyHidden}>
+                  <TableSortLabel
+                      active={orderBy === headCell.id}
+                      direction={orderBy === headCell.id ? order : 'asc'}
+                      onClick={createSortHandler(headCell.id)}
+                  >
+                    {headCell.label}
+                    {orderBy === headCell.id ? (
+                        <span className={classes.visuallyHidden}>
                   {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
                 </span>
-                  ) : null}
-                </TableSortLabel>
-              </TableCell>
-          ))}
-        </TableRow>
-      </TableHead>
-  );
-}
+                    ) : null}
+                  </TableSortLabel>
+                </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+    );
+  }
 
-EnhancedTableHead.propTypes = {
-  classes: PropTypes.object.isRequired,
-  numSelected: PropTypes.number.isRequired,
-  onRequestSort: PropTypes.func.isRequired,
-  onSelectAllClick: PropTypes.func.isRequired,
-  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
-  orderBy: PropTypes.string.isRequired,
-  rowCount: PropTypes.number.isRequired,
-};
+  EnhancedTableHead.propTypes = {
+    classes: PropTypes.object.isRequired,
+    numSelected: PropTypes.number.isRequired,
+    onRequestSort: PropTypes.func.isRequired,
+    onSelectAllClick: PropTypes.func.isRequired,
+    order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+    orderBy: PropTypes.string.isRequired,
+    rowCount: PropTypes.number.isRequired,
+  };
 
-const useToolbarStyles = makeStyles((theme) => ({
-  root: {
-    paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(1),
-  },
-  highlight:
-      theme.palette.type === 'light'
-          ? {
-            color: theme.palette.secondary.main,
-            backgroundColor: lighten(theme.palette.secondary.light, 0.85),
-          }
-          : {
-            color: theme.palette.text.primary,
-            backgroundColor: theme.palette.secondary.dark,
-          },
-  title: {
-    flex: '1 1 100%',
-  },
-}));
+  const useToolbarStyles = makeStyles((theme) => ({
+    root: {
+      paddingLeft: theme.spacing(2),
+      paddingRight: theme.spacing(1),
+    },
+    highlight:
+        theme.palette.type === 'light'
+            ? {
+              color: theme.palette.secondary.main,
+              backgroundColor: lighten(theme.palette.secondary.light, 0.85),
+            }
+            : {
+              color: theme.palette.text.primary,
+              backgroundColor: theme.palette.secondary.dark,
+            },
+    title: {
+      flex: '1 1 100%',
+    },
+  }));
 
-const EnhancedTableToolbar = (props) => {
-  const classes = useToolbarStyles();
-  const {numSelected} = props;
+  const EnhancedTableToolbar = (props) => {
+    const classes = useToolbarStyles();
+    const {numSelected} = props;
 
-  return (
-      <Toolbar
-          className={clsx(classes.root, {
-            [classes.highlight]: numSelected > 0,
-          })}
-      >
-        {numSelected > 0 ? (
-            <Typography className={classes.title} color="inherit" variant="subtitle1" component="div">
-              {numSelected} selected
-            </Typography>
-        ) : (
-            <Typography className={classes.title} variant="h6" id="tableTitle" component="div">
-              Nutrition
-            </Typography>
-        )}
+    return (
+        <Toolbar
+            className={clsx(classes.root, {
+              [classes.highlight]: numSelected > 0,
+            })}
+        >
+          {numSelected > 0 ? (
+              <Typography className={classes.title} color="inherit" variant="subtitle1" component="div">
+                {numSelected} selected
+              </Typography>
+          ) : (
+              <Typography className={classes.title} variant="h6" id="tableTitle" component="div">
+                Nutrition
+              </Typography>
+          )}
 
-        {numSelected > 0 ? (
-            <Tooltip title="Delete">
-              <IconButton aria-label="delete">
-                <DeleteIcon/>
-              </IconButton>
-            </Tooltip>
-        ) : (
-            <Tooltip title="Filter list">
-              <IconButton aria-label="filter list">
-                <FilterListIcon/>
-              </IconButton>
-            </Tooltip>
-        )}
-      </Toolbar>
-  );
-};
+          {numSelected > 0 ? (
+              <Tooltip title="Delete">
+                <IconButton aria-label="delete">
+                  <DeleteIcon/>
+                </IconButton>
+              </Tooltip>
+          ) : (
+              <Tooltip title="Filter list">
+                <IconButton aria-label="filter list">
+                  <FilterListIcon/>
+                </IconButton>
+              </Tooltip>
+          )}
+        </Toolbar>
+    );
+  };
 
-EnhancedTableToolbar.propTypes = {
-  numSelected: PropTypes.number.isRequired,
-};
+  EnhancedTableToolbar.propTypes = {
+    numSelected: PropTypes.number.isRequired,
+  };
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '100%',
-  },
-  paper: {
-    width: '100%',
-    marginBottom: theme.spacing(2),
-  },
-  table: {
-    minWidth: 750,
-  },
-  visuallyHidden: {
-    border: 0,
-    clip: 'rect(0 0 0 0)',
-    height: 1,
-    margin: -1,
-    overflow: 'hidden',
-    padding: 0,
-    position: 'absolute',
-    top: 20,
-    width: 1,
-  },
-}));
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      width: '100%',
+    },
+    paper: {
+      width: '100%',
+      marginBottom: theme.spacing(2),
+    },
+    table: {
+      minWidth: 750,
+    },
+    visuallyHidden: {
+      border: 0,
+      clip: 'rect(0 0 0 0)',
+      height: 1,
+      margin: -1,
+      overflow: 'hidden',
+      padding: 0,
+      position: 'absolute',
+      top: 20,
+      width: 1,
+    },
+  }));
 
-export default function EnhancedTable() {
   const classes = useStyles();
   const [order, setOrder] = React.useState('asc');
   const [orderBy, setOrderBy] = React.useState('calories');
@@ -355,3 +357,5 @@ export default function EnhancedTable() {
       </div>
   );
 }
+
+export default ArticleTable;
