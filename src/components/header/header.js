bf6273e5388748458c8fdfbdea87f0568980da5b
Ricardo Monteiro Simoes <ricardo_simoes_93@hotmail.com>
2020-05-05 22:14:44 +0200

    Refactors header to use UserContext

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,62 +1,71 @@
-import React, {useState, useEffect} from 'react';
+import React, { useContext, useEffect } from 'react';
+import {UserContext} from '../singlePage/singlePage';
 import Logo from '../header/logo.png';
 import { makeStyles } from '@material-ui/core/styles';
 
 import * as API from '../connectionHandler/connectionHandler'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCogs } from '@fortawesome/free-solid-svg-icons'
+import { IconButton } from '@material-ui/core';
 
-
-const Header = (onError) => {
-
-  const [username,setUsername] = useState(null);
+const Header = ({ onSettingsClick, onError }) => {
 
   const useStyles = makeStyles({
-    header:{
+    header: {
       height: '50px',
       width: '100vw',
       top: '0px',
       backgroundColor: '#2E75B6',
     },
-    logo:{
+    logo: {
       float: 'left',
     },
-    options:{
+    button: {
+      flexBasis: '8.33%',
+      float: 'right'
+    },
+    options: {
       float: 'right',
       paddingLeft: '10px',
       paddingRight: '5px',
       marginTop: '5px'
     },
-    username:{
+    username: {
       color: '#FFF',
       float: 'right',
       marginTop: '12px',
       marginRight: '12px'
-      //verticalAlign: 'top'
     },
-    headerRight:{
+    headerRight: {
       position: 'relative'
     }
   });
 
-  useEffect(() => {
-    API.getUserData(onError, parseUserName);
-  },[]);
-
-  const parseUserName = (userData) => {
-    setUsername(userData.firstName + ' ' + userData.lastName);
-  }
-
   const classes = useStyles();
 
-  return (
-      <div className={classes.header}>
-        <a href='/'>
-        <img className={classes.logo} src={Logo} alt={'Logo uniCalc'} height={'50px'}/>
-        </a>
-        <div className={classes.headerRight}>
-          <div className={classes.username}>{username}</div>
-        </div>
+  const user = useContext(UserContext);
 
+  const settings = user? 
+    (user.roles.includes("Admin") || user.roles.includes("Verkäufer"))?
+    <>
+      <div className={classes.username}>{user.firstName + ' ' + user.lastName}</div>
+      <IconButton className={classes.button} onClick={onSettingsClick}>
+        <FontAwesomeIcon icon={faCogs} />
+      </IconButton>
+    </>
+    :
+    <div className={classes.username}>{user.firstName + ' ' + user.lastName}</div>
+    : null;
+
+  return (
+    <div className={classes.header}>
+      <a href='/'>
+        <img className={classes.logo} src={Logo} alt={'Logo uniCalc'} height={'50px'} />
+      </a>
+      <div className={classes.headerRight}>
+        {settings}
       </div>
+    </div>
   );
 };
 
