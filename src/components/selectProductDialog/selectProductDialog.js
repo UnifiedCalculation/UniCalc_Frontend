ec77f2aa91fe3f8cf7100e684de74eccb83159cf
Ricardo Monteiro Simoes <ricardo_simoes_93@hotmail.com>
2020-05-09 20:37:47 +0200

    Changes parsing for new object structure

--- src/components/selectProductDialog/selectProductDialog.js
+++ src/components/selectProductDialog/selectProductDialog.js
@@ -81,7 +81,10 @@ const SelectProductDialog = ({ products, onCancel, onSubmit, show, ...props }) =
     const parseNewProduct = (jsonObject) => {
         jsonObject.discount = parseInt(jsonObject.discount);
         jsonObject.amount = parseInt(jsonObject.amount);
-        jsonObject.article_id = products.find(product => product.name == jsonObject.article_id).id;
+        console.log(jsonObject);
+        console.log(products);
+        jsonObject.article = products.find(product => product.name == jsonObject.article).id;
+        delete jsonObject.article;
         onSubmit(jsonObject);
     };
 
